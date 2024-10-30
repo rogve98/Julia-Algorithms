4c60211025b6bd2f7eaf71a42563a7f931904103
unknown <rogve98>
2024-10-30 16:45:58 -0600

    Se agregan 3 ejercicios más a la lista, y se enlistan en el readme para que se puedan cosultar.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Julia-Algorithms
 Se incluye un archivo main.jl con la lista de todos los algoritmos que vaya resolviendo del proyecto Euler. Con el tiempo quizás iré subiendo más cosas que puedan ser interesantes.
+
+1. multiplos3y5(): Dado un número se brindan todos sus múltiplos en el rango de 1 a dicho número
+2. imparesFibonacci(): Devuelve todos los términos impartes de la serie de Fibonacci
+3. factoresPrimos(): Devuelve todos los factores primos de un número dado
+bonus 0. ispalindrome(): devuelve verdadero o falso testeando un número si es palíndromo.
+bonus 1. permuteVect(): brinda una permutación hacia la derecha
+bonus 2. factoresde2(): brinda 2 factores de dos dígitos dado un número
+bonus 3. factoresde3(): brinda 2 factores de tres dígitos dado número
+bonus 4. largestpalindrome(): brinda el palíndromo más grande como producto de dos factores de 2 y 3 dígitos (es ineficiente)
+4. largestPalindrome(): palíndromo más grande como producto de dos números de 3 dígitos.
+bonus 5. isprime(): Sirve para determinar si un número dado es primo.
+5. smallestFactors(): determina el número más pequeño formado por factores del 1 al 20.
+6. sumSquareDifference(): Resta de la suma del 1 al 20 del cuadrado con los cuadrados del 1 al 20.
+bonus 6. primosHastaN(): Determina el conjunto de números primos dada una cota superior N.
+7. primos(): Regresa una lista con los N+1 primeros primos.i
+8. greatestFactor1000(): Dada una lista de 1000 dígitos enteros, se encuentra el producto más grande de un slice de 13 factores.
+
--- main.jl
+++ main.jl
@@ -12,7 +12,11 @@ bonus 3. factoresde3(): brinda 2 factores de tres dígitos dado número
 bonus 4. largestpalindrome(): brinda el palíndromo más grande como producto de dos factores de 2 y 3 dígitos (es ineficiente)
 4. largestPalindrome(): palíndromo más grande como producto de dos números de 3 dígitos.
 bonus 5. isprime(): Sirve para determinar si un número dado es primo.
-5. smallestFactors(): determina el número más pequeño formado por factores del 1 al 20. 
+5. smallestFactors(): determina el número más pequeño formado por factores del 1 al 20.
+6. sumSquareDifference(): Resta de la suma del 1 al 20 del cuadrado con los cuadrados del 1 al 20.
+bonus 6. primosHastaN(): Determina el conjunto de números primos dada una cota superior N.
+7. primos(): Regresa una lista con los N+1 primeros primos.i
+8. greatestFactor1000(): Dada una lista de 1000 dígitos enteros, se encuentra el producto más grande de un slice de 13 factores.
 """
 
 #Múltiplos de 3 y 5: Completado
@@ -187,6 +191,78 @@ function smallestFactors(n::Int)
     end
     return resultado#prod(resultado)
 end
+           
+#Este estuvo fácil. Resta de la suma del 1 al 20 del cuadrado con los cuadrados del 1 al 20: Completado
             
-            
-              
+function sumSquareDifference(n::Int)
+    cuadrados = 0
+    cuadrado = 0
+    for i in 1:n
+        cuadrados+=i^2
+        cuadrado+=i
+    end
+    cuadrado = cuadrado^2
+    return cuadrado - cuadrados
+end
+
+#Primos hasta una cota superior N, devuelve una lista de primos truncada hasta cierto número N
+
+function primosHastaN(N::Int)
+    primos = [2]
+    for i in 3:N
+        es_primo = true
+        limite = sqrt(i)
+        for primo in primos
+            if primo > limite
+                break
+            end
+            if mod(i,primo) == 0
+                es_primo = false
+                break
+            end
+        end
+        if es_primo
+            push!(primos,i)
+        end
+    end
+    return primos
+end
+
+#Primo número 10 001
+
+function primos(N::Int=10_000)
+    primos = [2]
+    i = 3
+    while length(primos) <= N
+        es_primo = true
+        limite = isqrt(i)
+        for primo in primos
+            if primo > limite break end
+            if mod(i,primo) == 0
+                es_primo = false
+                break
+            end
+        end
+        if es_primo push!(primos,i) end
+        i+=1
+    end
+    return primos
+end
+
+#Función para determinar el producto más grande dados 4 factores en un número de 1000 dígitos.
+
+function greatestFactor1000(num::String,j::Int)
+    num = replace(num,"\n" => "")
+    greatest = 0
+    fs = [[7,3,1,6]]
+    for i in 1:length(num)-j
+        factores = num[i:i+j-1]
+        lista = [parse(Int,s) for s in factores]
+        if prod(lista) > greatest
+            greatest = prod(lista)
+            fs = replace(fs,fs[1] => lista)
+        end
+    end
+    return greatest,fs           
+end
+
